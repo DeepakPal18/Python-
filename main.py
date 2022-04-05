7cc59c160c93c2c6ae315d934c725bfa60b27b93
Deepak Pal <48427419+DeepakPal18@users.noreply.github.com>
2022-04-05 21:42:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age in years? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+years = 90 - int(age)
+print(f"years left: {years}")
+days = (years * 365)
+print(f"days left: {days}")
+weeks = (years * 52)
+print(f"weeks left: {weeks}")
+months = (years * 12)
+print(f"months left: {months}")
+#print(f"You have {days} days, {weeks} weeks, and {months} months left")
+
+
+
+
+
+
+
